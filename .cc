f18fe84b4df1eb165d1f5b7f0b70446701ac962c
Ha.Minh <minhhh@minhhuyha.info>
2014-02-09 16:47:45 +0700

    feat: update cc

--- .cc
+++ .cc
@@ -1,21 +1,25 @@
-# Using sed and awk to filter command line output
-ll | sed -n "1,10p" | awk '{print $0}'
+# Git
+    git fetch -p
+    git submodule add <git@github ...> snipmate-snippets/snippets/
 
-# ack file
-ack -Q --smart-case --js "under_dungeon" --ignore-directory=build --ignore-file=is:packed_us.js --ignore-file=is:packed_jp.js
+### Using sed and awk to filter command line output
+    ll | sed -n "1,10p" | awk '{print $0}'
 
-# Copy using rsync
-rsync -lav -P source dest
+### ack file
+    ack -Q --smart-case --js --ignore-file=match:/packed.*\.js/ --ignore-file=is:Code/tag --ignore-dir=build --ignore-dir=Code/JSON --ignore-dir=Tools "test"
 
-# Map localhost port to some server port
-ssh -L localhost:3306:192.168.56.201:3306 ubuntu@xx.xx.xx.xx -i ~/.ssh/id_rsa
+### Copy using rsync
+    rsync -lav -P source dest
 
-# Map localhost port to some other port
-ssh -L localhost:3306:192.168.56.201:3306 -N 127.0.0.1
+### Map localhost port to some server port
+    ssh -L localhost:3306:192.168.56.201:3306 ubuntu@xx.xx.xx.xx -i ~/.ssh/id_rsa
 
-# Calculation
-echo "ibase=10;obase=16; 255"|bc -l
-echo "5/3" | bc -l
+### Map localhost port to some other port
+    ssh -L localhost:3306:192.168.56.201:3306 -N 127.0.0.1
 
-# Using xargs and find and awk
-find . -name "*.bak" -print0 | awk '{print $0}' | xargs -0 -I {} mv {} ~/old.files
+### Calculation
+    echo "ibase=10;obase=16; 255"|bc -l
+    echo "5/3" | bc -l
+
+### Using xargs and find and awk
+    find . -name "*.bak" -print0 | awk '{print $0}' | xargs -0 -I {} mv {} ~/old.files
