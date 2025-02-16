44cefa5db0186fd58792ca4386c39a756f8cba7d
Ha.Minh <minhhh@minhhuyha.info>
2025-02-16 10:21:54 -0500

    feat: update nix

--- nix/env.nix
+++ nix/env.nix
@@ -1,4 +1,7 @@
 with import <nixpkgs> {}; [
+    premake5
+    fd
+    neovim
     grpc
     cmake
     cmake-format
@@ -11,48 +14,53 @@ with import <nixpkgs> {}; [
     ripgrep
     lua
     llama
-    fzf
     ack
     silver-searcher
-    adobe-reader 
-    astyle 
-    ffmpeg_6 
-    figlet 
-    flex 
-    go 
-    goku 
-    guile_3_0 
-    htop 
-    hugo 
-    imagemagick 
-    jrnl 
-    multimarkdown 
-    par2cmdline 
-    pngcrush 
-    pngquant 
-    putty 
-    zeromq 
-    zlib 
-    figlet 
-    jrnl 
-    advancecomp 
-    stripe-cli 
-    glew 
-    nasm 
-    postgresql 
-    optipng 
-    fastlane 
-    ncurses 
-    redis 
-    rsync 
-    snappy 
-    mercurial 
-    SDL 
-    SDL2 
-    p7zip 
-    hashdeep 
-    pandoc 
-    racket 
+    astyle
+    ffmpeg_6
+    figlet
+    flex
+    go
+    goku
+    guile_3_0
+    htop
+    hugo
+    imagemagick
+    jrnl
+    multimarkdown
+    par2cmdline
+    pngcrush
+    pngquant
+    putty
+    zeromq
+    zlib
+    figlet
+    jrnl
+    advancecomp
+    stripe-cli
+    glew
+    nasm
+    postgresql
+    optipng
+    fastlane
+    ncurses
+    redis
+    rsync
+    snappy
+    mercurial
+    SDL
+    SDL2
+    p7zip
+    hashdeep
+    pandoc
+    racket
     slack
     tmux
+    curl
+    xquartz
+    imagemagick
+    gtk2
+    rclone
+    libuv
+    zoxide
 ]
