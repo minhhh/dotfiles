1ddc5640b77cf731d600abe58e1cd7fd330a79f5
Ha.Minh <minhhh@minhhuyha.info>
2026-06-08 16:59:08 -0400

    style(nvim): update markdown heading highlights for onelight and onedark

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -24,6 +24,10 @@ end
 
 vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#ff0000', bg = 'none' })
 
-if colorscheme == "onelight" or colorscheme == "onedark" then
-  vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { fg = '#d46a7a', bold = true })
+if colorscheme == "onelight" then
+  vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { fg = '#118dc3', bold = true })
+  vim.api.nvim_set_hl(0, '@markup.heading.5.markdown', { fg = '#56b6c2', bold = true })
+elseif colorscheme == "onedark" then
+  vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { fg = '#61afef', bold = true })
+  vim.api.nvim_set_hl(0, '@markup.heading.5.markdown', { fg = '#56b6c2', bold = true })
 end
