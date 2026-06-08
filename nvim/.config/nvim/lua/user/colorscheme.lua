1c4778a28fec2b710c3f18563c98b7bf401cc079
Ha.Minh <minhhh@minhhuyha.info>
2026-06-08 14:22:28 -0400

    feat(colorscheme): switch to onelight and configure light themes

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,21 @@
-local colorscheme = "PaperColor"
+local colorscheme = "onelight"
+
+if colorscheme == "catppuccin" then
+  local ok, _ = pcall(require, "catppuccin")
+  if ok then
+    require("catppuccin").setup({ flavour = "latte" })
+  end
+elseif colorscheme == "onedark" or colorscheme == "onelight" then
+  local ok, _ = pcall(require, "onedarkpro")
+  if ok then
+    require("onedarkpro").setup({})
+  end
+elseif colorscheme == "onenord" then
+  local ok, _ = pcall(require, "onenord")
+  if ok then
+    require("onenord").setup({ theme = "light" })
+  end
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
@@ -6,3 +23,7 @@ if not status_ok then
 end
 
 vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#ff0000', bg = 'none' })
+
+if colorscheme == "onelight" or colorscheme == "onedark" then
+  vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { fg = '#d46a7a', bold = true })
+end
